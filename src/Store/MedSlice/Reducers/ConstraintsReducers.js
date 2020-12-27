bf02945e0c798e97f5429c04023d32274814513b
runxuanjiang <runxuan.jiang@gmail.com>
2020-12-26 21:21:47 -0500

    added function signatures for constraints reducers

---
+++ src/Store/MedSlice/Reducers/ConstraintsReducers.js
@@ -0,0 +1,55 @@
+function addConstraintsSetReducer(constraints) {
+
+}
+
+function updateConstraintsWeightsReducer(constraints, setIndex) {
+
+}
+
+function addSingleMinConstraintReducer(constraints, setIndex) {
+
+}
+
+function addSingleMaxConstraintReducer(constraints, setIndex) {
+
+}
+
+function addCumulativeMinConstraintReducer(constraints, setIndex) {
+
+}
+
+function addCumulativeMaxConstraintReducer(constraints, setIndex) {
+
+}
+
+function deleteSingleMinConstraintReducer(constraints, setIndex) {
+
+}
+
+function deleteSingleMaxConstraintReducer(constraints, setIndex) {
+
+}
+
+function deleteCumulativeMinConstraintReducer(constraints, setIndex) {
+
+}
+
+function deleteCumulativeMaxConstraintReducer(constraints, setIndex) {
+
+}
+
+function updateSingleMinConstraintReducer(constraints, setIndex, amount) {
+
+}
+
+function updateSingleMaxConstraintReducer(constraints, setIndex, amount) {
+
+}
+
+function updateCumulativeMinConstraintReducer(constraints, setIndex, amount) {
+
+}
+
+function updateCumulativeMaxConstraintReducer(constraints, setIndex, amount) {
+
+}
