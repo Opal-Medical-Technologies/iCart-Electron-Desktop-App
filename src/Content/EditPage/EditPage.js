6d6c9eba09991466f477cc98bc51d00e1fc7f4c0
runxuanjiang <runxuan.jiang@gmail.com>
2020-12-31 18:18:29 -0500

    light formatting on preview pane, added main components to middlebar

--- src/Content/EditPage/EditPage.js
+++ src/Content/EditPage/EditPage.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './EditPage.css';
 import PreviewPane from './PreviewPane/PreviewPane';
+import InputPane from './InputPane/InputPane';
 
 export default function EditPage() {
     return (
         <div className = 'EditPage_Base'>
-            <div className = 'EditPage_CenterPanel'></div>
+            <div className = 'EditPage_CenterPanel'>
+                <InputPane/>
+            </div>
             <div className = 'EditPage_RightPanel'>
                 <PreviewPane/>
             </div>
--- src/Content/EditPage/InputPane/InputPane.js
+++ src/Content/EditPage/InputPane/InputPane.js
@@ -1,10 +1,7 @@
 import {React, useState} from 'react';
-import "./MiddleBar.css";
-// import "./Widget.css";
-// import { ConstraintsCheckbox } from './Pages/Component/MiddleBar/ConstraintsCheckbox'
-// import { NotesLine } from './Pages/Component/MiddleBar/NotesLine'
-
 import { useSelector, useDispatch } from 'react-redux'
+
+import "./InputPane.css";
 import { 
     selectMed,
     updateMedName, 
@@ -43,139 +40,73 @@ import {
     deleteNotesSet,
     updateNotesWeights,
     updateNotesText,
-} from '../../../../Store/MedSlice/MedSlice';
+} from '../../../Store/MedSlice/MedSlice';
 
-/*
 const units = ['mg', 'g', 'mcg', 'mEq', 'Eq'];
 
-function renderUnitButtons(activeUnits, dispatch) {
+
+export default function InputPane() {
+    const medData = useSelector(selectMed);
+    const dispatch = useDispatch();
+    console.log(medData)
+
     return(
-        <div>
-            {units.map(unit => (
-                <button className={unit == activeUnits ? "Active-Unit-Button" : "Inactive-Unit-Button"} onClick = {() => dispatch(updateMedUnits(unit))}>
-                    {unit}
-                </button>
-            ))}
+        <div className = "InputPane">
+            {TitleBox(medData, dispatch)}
+            {UnitsBox(medData, dispatch)}
+            {ConcentrationBox(medData, dispatch)}
+        </div>
+    );
+}
+
+function TitleBox(medData, dispatch) {
+    return (
+        <div className="InputPane_TitleBox">
+            <input className="InputPane_TitleBox_Name" type='text' value={medData.name} onChange={e => dispatch(updateMedName(e.target.value))}/>
+            <div className="InputPane_TitleBox_FolderPath">
+                ACTIVE/MEDICATIONS
+            </div>
+            <hr className="InputPane_TitleBox_Divider"/>
         </div>
     )
 }
 
-function UnitsInput(props) {
-    const medData = useSelector(selectMed);
-    const dispatch = useDispatch();
-    return(
-        <div className = "UnitsWidgetInput">
-            {renderUnitButtons(medData.units, dispatch)}
-        </div>
-    );
-}
-
-function ConcentrationInput(props) {
-    const medData = useSelector(selectMed);
-    const dispatch = useDispatch();
-    return(
-        <div className = "ConcentrationWidgetInput">
-            <input type = 'text' value = {medData.conc} onChange = {e => dispatch(updateMedConc(e.target.value))} style={{ width: "30.57vw", textAlign: 'right' } }/>
-            <label> {medData.units}/kg</label>
-        </div>
-    );
-}
-const StandardCustomButton = ({onClick, status}) => {
+function UnitsBox(medData, dispatch) {
     return (
-      <button className = "StandardCustomButton" onClick={onClick}>
-        {`${status ? 'STANDARD' : 'CUSTOM'}`}
-      </button>
-    );
-  };
-
-function Widget(props) {
-
-  const [status, setStatus] = useState(true);
-  const updateStatus = () => {
-    setStatus(!status);
-  }
-
-  return(
-      <div className = "Widget">
-        <div className = "Widget_TopRow">
-          <div className = "Widget_TopRow_Left">
-            <h1 className = "Widget_Title">
-              {props.header}
-            </h1>
-          </div>
-          <div className = "Widget_TopRow_Right">
-            { props.toggle ?  
-            ( <StandardCustomButton onClick = {updateStatus} status = {status} /> ) : (<div/>)} 
-          </div>
-        </div>
-        <div className = "Widget_Divider"/>
-        { status ? ( <Child input = {props.input} /> ) : (props.toggle ? ( <Parent input = {props.input} /> ) : (<div/>)) }
-      </div> 
-  );
-
-}
-*/
-
-export default function MiddleBar() {
-    const medData = useSelector(selectMed);
-    const dispatch = useDispatch();
-    console.log(medData);
-
-    return(
-      <div className = "MiddleBar_Base">
-        <div className = "MiddleBar_Padding">
-          <div className = "MiddleBar_TopPanel">
-            <div className = "MiddleBar_TopPanel_Left">
-              <input className = "MiddleBar_DrugName"
-                    type = 'text' 
-                    value={medData.name} 
-                    onChange={e => dispatch(updateMedName(e.target.value))}
-              />
-              <text className = "MiddleBar_FolderName">ACTIVE/MEDICATIONS</text>
+        <div className="InputPane_WidgetBox">
+            <div className="InputPane_WidgetBox_Title">
+                Units:
             </div>
-            <div className = "MiddleBar_TopPanel_Right">
-                <button className = "MiddleBar_DeleteButton">DELETE</button>
-                <button className = "MiddleBar_SaveButton">SAVE</button>
+            <hr className="InputPane_WidgetBox_Divider"/>
+            <div className = "InputPane_UnitsBox_Units">
+                {units.map(unit => (
+                    <button className={unit == medData.units ? "Active-Unit-Button" : "Inactive-Unit-Button"} onClick = {() => dispatch(updateMedUnits(unit))}>
+                        {unit}
+                    </button>
+                ))}
             </div>
-          </div>
-          <hr className = "MiddleBar_Divider"></hr>
-          {/* Place widgets directly here */}
         </div>
-      </div>
     )
 }
 
-function Child(props) {
-    return (props.input); 
-  }
-  
-  function Parent(props) {
-    const [lines, setLines] = useState([0]);  
+function ConcentrationBox(medData, dispatch) {
     return (
-      <div>
-        <div>
-        <button className = "StandardCustomButton" onClick={() => {setLines([...lines, lines.length]); }}>ADD</button>
-        </div>
-        <div>
-        {lines.map(m => <Child input = {props.input} pstate={{lines, setLines}}/>)}
-      </div>
-      </div>
-    );
-  }
+        <div className="InputPane_WidgetBox">
+            <div className="InputPane_WidgetBox_Title">
+                Concentration:
+            </div>
+            <hr className="InputPane_WidgetBox_Divider"/>
+            <div className = 'InputPane_ConcentrationBox_Input'>
+                <input type ='text' value = {medData.conc} onChange = {e => dispatch(updateMedConc(e.target.value))} style={{ width: "3.5vw", textAlign: 'center' } }/>
+            </div>
+            <div className = "InputPane_ConcentrationBox_Units">
+                {medData.units}/mL
+            </div>
 
-  /*
-  return(
-        <div className = "MiddleBar">
-            <div className = "MiddleBar_Title">
-            
-                <div className = "FolderPath">
-                    ACTIVE/MEDICATIONS
-                </div>
-                <div className = "Divider"></div>
-            </div>
-            <div className = "MiddleBar_Body">
-                <Widget className = "ConcentrationWidget" header = "Units" input = {<UnitsInput/>} toggle/>
-            </div>
         </div>
-    );
-  */
+    )
+}
+
+function DosageBox(medDat, dispatch) {
+
+}
