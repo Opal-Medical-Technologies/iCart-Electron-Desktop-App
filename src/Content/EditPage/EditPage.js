0e6de532ada7514289f74a98330f786006e3c23e
runxuanjiang <runxuan.jiang@gmail.com>
2021-01-01 13:44:41 -0500

    fixed bug with deleting all medications

--- src/Content/EditPage/EditPage.js
+++ src/Content/EditPage/EditPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
+import {selectInfo} from '../../Store/MedSlice/MedSlice'
 
 import './EditPage.css';
 import PreviewPane from './PreviewPane/PreviewPane';
 import InputPane from './InputPane/InputPane';
 
 export default function EditPage() {
-    return (
+    const medInfo = useSelector(selectInfo)
+    return (medInfo.medList.length > 0) ? (
         <div className = 'EditPage_Base'>
             <div className = 'EditPage_CenterPanel'>
                 <InputPane/>
@@ -13,6 +16,11 @@ export default function EditPage() {
             <div className = 'EditPage_RightPanel'>
                 <PreviewPane/>
             </div>
+        </div>
+    ) 
+    : (
+        <div className="EditPage_Empty">
+            Press + to add a medication.
         </div>
     )
 }
