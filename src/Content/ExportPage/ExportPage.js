00a633e8cd625ad9eff0a0c947e43fdc53d0c14a
crosscountryboy123 <crosscountryboy123@gmail.com>
2021-01-12 00:30:16 -0500

    New button prototype in ExportPage

--- src/Content/ExportPage/ExportPage.js
+++ src/Content/ExportPage/ExportPage.js
@@ -1,13 +1,48 @@
 import styled from 'styled-components';
-import Breathe from './CoolButton.js';
+
+import {FiEdit} from 'react-icons/fi';
+import './ExportPage.css'
+
+const StyledExportPage = styled.div`
+    float: right;
+`
+
+
+const StyledExpandingButton = styled.button`
+    
+    &span {
+        max-width: 0;
+        -webkit-transition: max-width 1s;
+        transition: max-width 1s;
+        display: inline-block;
+        vertical-align: top;
+        white-space: nowrap;
+        overflow: hidden;
+    }
+
+    &:hover span {
+        max-width: 7rem;
+        color: blue;
+    }
+
+    &:focus span {
+        color: green;
+    }
+
+
+
+`;
+
 
 
 export default function ExportPage() {
     return (
-        <div>
-            {"Export Page Placeholder"}
-            <Breathe/>
-        </div>
+        <StyledExportPage>
+            <StyledExpandingButton>
+                <span>Different text</span>
+                <FiEdit/>
+            </StyledExpandingButton>
+        </StyledExportPage>
     )
 }
 
